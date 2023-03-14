443d80959a249c9157132507795a170fa85968e3
Yuhancheng1 <127289588+Yuhancheng1@users.noreply.github.com>
2023-03-14 21:03:47 +0800

    Update hexagons.py

--- pratical5/hexagons.py
+++ pratical5/hexagons.py
@@ -1,1 +1,7 @@
+#we should use a while loop to cauculate the h when n keep changing,then change the Formula language to the programming language
+n=1
+while n<=5:
+    h=2*n*(2*n-1)*0.5
+    n=n+1
+    print(h)
 
