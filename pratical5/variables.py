fb22bae9dcc691d381f05f24bd11ec0288b426e1
Yuhancheng1 <127289588+Yuhancheng1@users.noreply.github.com>
2023-03-14 20:32:42 +0800

    Update variables.py

--- pratical5/variables.py
+++ pratical5/variables.py
@@ -1,1 +1,17 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=c-a
+if d>e:
+        print("e is better")
+elif e>d:
+        print("d is better")
+else:
+        print("d is same to e")
 
+   X=True
+Y=False
+W=X and Y
+Z=X or Y
+print (W,Z)
