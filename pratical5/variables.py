75c9b1a547901d16efe4399b1e096ae3eb6c2302
Yuhancheng1 <127289588+Yuhancheng1@users.noreply.github.com>
2023-03-14 20:44:33 +0800

    Update variables.py

--- pratical5/variables.py
+++ pratical5/variables.py
@@ -1,3 +1,8 @@
+# a is the longitude of Edinburgh 
+#b is the longitude of  Los Angeles
+#c is the longitude of Haining
+#d is the longitude difference	between	a and b	
+#e is the longitude difference	between	a and c
 a=-3.19
 b=-118.24
 c=116.39
@@ -10,8 +15,10 @@ elif e>d:
 else:
         print("d is same to e")
 # so Rob travel to Haining is further than to Los Angeles
-   X=True
+
+X=True
 Y=False
 W=X and Y
 Z=X or Y
 print (W,Z)
+#W means Flause,and Z means True
