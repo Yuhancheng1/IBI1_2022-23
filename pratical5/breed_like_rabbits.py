b155e3d52201ebce3fa8c2c34f70acd85846b556
Yuhancheng1 <127289588+Yuhancheng1@users.noreply.github.com>
2023-03-15 15:02:01 +0800

    Update breed_like_rabbits.py

--- pratical5/breed_like_rabbits.py
+++ pratical5/breed_like_rabbits.py
@@ -1,8 +1,8 @@
-# n means the total	number of rabbits that will be born each generation,m means the number of whole rabbits,j means the rabbit generation
+# n means the total	number of rabbits that will be born each generation,m means the number of whole rabbits,j means the rabbit generation，we use a while loop so that when the number of rabbits over 100 we can find it。
 j=1;n=0;m=2
 while m<100:
-    j=j+1
-    n=m
+    j=j+1  
+    n=m    
     m=m+n
 print(str(j),str(m))
 #the result show in the 7 generation, there will be total 128 rabitts
